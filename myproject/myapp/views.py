1d8515ddd688f6e41537f5278172dd4fbc99e034
mooon33 <164473198+mooon33@users.noreply.github.com>
2025-04-16 20:48:24 +1000

    занаыо

--- myproject/myapp/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Добро пожаловать на мой сайт!")
-def catalog_view(request):
-    return HttpResponse("Добро пожаловать на каталог")
